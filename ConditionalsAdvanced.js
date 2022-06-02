eede7a832a3fa55efcf4dcc1ce3b007eaaa0db02
David Aror <68910912+Teji360@users.noreply.github.com>
2022-06-02 16:53:56 -0400

    Add files via upload

---
+++ ConditionalsAdvanced.js
@@ -0,0 +1,7 @@
+age = 16
+const diet = "Kale"
+if((age > 15) && (diet=="Kale")){
+    console.log("you are going to be strong one day")
+}else{
+    console.log("fail")
+}
---
+++ Conditonals.js
@@ -0,0 +1,4 @@
+age = 16;
+if(age > 15){
+    console.log("You are older than 15!")
+}
