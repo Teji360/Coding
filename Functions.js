2313ff9137f8891db32c97496cf4faa97896b697
David Aror <68910912+Teji360@users.noreply.github.com>
2022-06-03 12:02:19 -0400

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,11 @@
+function multiply(num1, num2) {
+    return num1 * num2 
+}
+function destroy(num3, num4){
+    if(num3 != num4 ){
+        num3 = num4
+    }
+    return num3 - num4
+}
+console.log(destroy(100,10124012742108))
+console.log(multiply(5,5))
