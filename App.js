95f4ffbc8f6a67754d8cd33953fc4e8db7c6c36d
David Aror <68910912+Teji360@users.noreply.github.com>
2022-06-03 13:23:07 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import React, { useState } from 'react'
+function App() {
+  const [count, setCount] =useState(0);
+  return (
+    <div>
+      <p>You have clicked me {count} times </p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Click Me!
+      </button>
+   </div>
+  );
+}
+
+export default App;
